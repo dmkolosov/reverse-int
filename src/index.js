7b2573e3d45d1a76c020dc298aa9135bf0493ede
dmkolosov <maker.bug.00@inbox.ru>
2020-10-20 23:07:56 +0300

    refactor: del comments

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 module.exports = function reverse (n) {
-    let flag = Math.sign(n)
-
     let str=Math.abs(n).toString()
-    console.log(str)
-
     let number =str.split("").reverse().join("")
-    // let number =flag>0?str.split("").reverse().join(""):"-"+str.split("").reverse().join("")
-
+    
     return  parseInt(number)
 
 }
